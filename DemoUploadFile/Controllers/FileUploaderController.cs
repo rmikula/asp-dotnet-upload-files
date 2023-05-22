00c355de61070699ffe5661a557a673ed3a4f6f0
Roman Mikula <roman.mikula@embedit.cz>
2023-05-22 15:42:12 +0200

    More usecases.

--- DemoUploadFile/Controllers/FileUploaderController.cs
+++ DemoUploadFile/Controllers/FileUploaderController.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
 
 namespace DemoUploadFile.Controllers
 {
@@ -12,14 +17,18 @@ namespace DemoUploadFile.Controllers
     public class FileUploaderController : ControllerBase
     {
         private const long MaxFileSize = 10L * 1024L * 1024L * 1024L; // 10GB, adjust to your need;
-        
-        private readonly ILogger<FileUploaderController> _logger;
 
-        public FileUploaderController(ILogger<FileUploaderController> logger)
+        private readonly ILogger<FileUploaderController> _logger;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly IAzureClientFactory<BlobServiceClient> _azureClientFactory;
+
+        public FileUploaderController(ILogger<FileUploaderController> logger, BlobServiceClient blobServiceClient, IAzureClientFactory<BlobServiceClient> azureClientFactory)
         {
             _logger = logger;
+            _blobServiceClient = blobServiceClient;
+            _azureClientFactory = azureClientFactory;
         }
-        
+
         /// <summary>
         /// I can use firstFile or secondFile directly or iterate through Request.Form.Files collection
         /// </summary>
@@ -29,17 +38,54 @@ namespace DemoUploadFile.Controllers
         [RequestFormLimits(MultipartBodyLengthLimit = MaxFileSize)]
         public async Task PostSingleFile([FromForm] IFormFile firstFile, IFormFile secondFile)
         {
-
             foreach (var formFile in Request.Form.Files)
             {
-                Console.WriteLine($"Field name: {formFile.Name}, FileName: {formFile.FileName}, Size: {formFile.Length} contentType: {formFile.ContentType} ");
+                Console.WriteLine(
+                    $"Field name: {formFile.Name}, FileName: {formFile.FileName}, Size: {formFile.Length} contentType: {formFile.ContentType} ");
 
                 await using var stream = new FileStream($"uploads/{formFile.FileName}", FileMode.Create);
-                
+
                 await formFile.CopyToAsync(stream);
             }
         }
 
-        
+        [HttpPost("toBlob")]
+        public async Task PostFileToAzureBlob([FromForm] IFormFile firstFile)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("files");
+
+            // Check if container exists
+            var response = await containerClient.CreateIfNotExistsAsync();
+
+            var blobClient = containerClient.GetBlobClient(firstFile.FileName);
+
+            // set http headers
+            var headers = new BlobHttpHeaders()
+            {
+                ContentType = firstFile.ContentType,
+            };
+
+            var uploadAsync = await blobClient.UploadAsync(content: firstFile.OpenReadStream(), headers);
+
+            // Response<BlobContentInfo>? uploadAsync = await blobClient.UploadAsync(content: firstFile.OpenReadStream(), overwrite: true);
+            // await blobClient.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = firstFile.ContentType });
+
+            // await containerClient.UploadBlobAsync(firstFile.FileName, firstFile.OpenReadStream());
+
+            Accepted();
+        }
+
+        [HttpPost("toBlob2")]
+        public async Task PostFileToAzureBlob2([FromForm] IFormFile firstFiles)
+        {
+            var client = _azureClientFactory.CreateClient("RoMikVersion");
+
+            var containerClient = client.GetBlobContainerClient("files");
+            
+            foreach (var blobItem in containerClient.GetBlobs())
+            {
+                Console.WriteLine($"BlobName: {blobItem.Name} contentType: {blobItem.Properties.ContentType}");
+            }
+        }
     }
 }
--- DemoUploadFile/Program.cs
+++ DemoUploadFile/Program.cs
@@ -1,11 +1,32 @@
+using Azure.Identity;
+using Microsoft.Extensions.Azure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddSecretClient(vaultUri: new Uri("https://romik-kv.vault.azure.net/"));
+
+    var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+    {
+        ManagedIdentityClientId = "9f49b200-a820-47bd-bf8f-709ecbc9b4c9",
+    });
+
+    clientBuilder.AddBlobServiceClient(new Uri("https://romikstorage.blob.core.windows.net/")).WithName("RoMikVersion");
+        // .WithCredential(credential);
+
+
+    clientBuilder.AddBlobServiceClient(serviceUri: new Uri("https://romikstorage.blob.core.windows.net/"));
+    clientBuilder.UseCredential(new DefaultAzureCredential());
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
